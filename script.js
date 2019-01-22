d1050d98a3e6b3df95da952ded13a32abfe17a8a
clementsem <clementctes@gmail.com>
2019-01-22 09:18:09 +0100

    jsex6

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+
+  var num1 = document.getElementById("firstNumber").value;
+  var num2 = document.getElementById("secondNumber").value;
+  var result = num1 % num2;
+  alert(result);
+}
